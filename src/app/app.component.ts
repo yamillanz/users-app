a37ca4b40d7ceb785439240650655957c0c16525
Yamil LANZ <yamil.lanz@naranjax.com>
2023-12-21 16:18:34 -0400

    adds all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PrimeBlocksModule } from './components/primeblocks/primeblocks.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PrimeBlocksModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'poc-a17-primeng';
