c2771b17d359aeda736450080ecefd0fcaf4b13f
Yamil LANZ <yamil.lanz@naranjax.com>
2023-12-25 20:11:49 -0400

    adds roueter to see teh blocks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { BlocksComponent } from './components/primeblocks/blocks/blocks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: BlocksComponent }];
