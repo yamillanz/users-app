da9d6037dcc77ebe25ae93f985dfa8e1f578137b
Yamil LANZ <yamil.lanz@naranjax.com>
2024-01-16 10:07:54 -0400

    app configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
+import { LayoutService, AppConfig } from '../app/services/app.layout.service';
 // import { PrimeBlocksModule } from './components/primeblocks/primeblocks.module';
 
 @Component({
@@ -13,8 +14,19 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
   primengConfig = inject(PrimeNGConfig);
+  layoutService = inject(LayoutService);
+
   ngOnInit(): void {
+    const config: AppConfig = {
+      ripple: true,
+      inputStyle: 'outlined',
+      menuMode: 'overlay',
+      colorScheme: 'dark',
+      theme: 'saga-blue',
+      scale: 14,
+    };
     this.primengConfig.ripple = true;
+    this.layoutService.config = config;
   }
   title = 'fi-app';
 }
