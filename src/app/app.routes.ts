b5d086372c9b360ea958116d2924b65607757c47
Yamil LANZ <yamil.lanz@naranjax.com>
2024-01-23 14:26:07 -0400

    no feedback

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,7 @@ import { authGuard } from './shared/guards/auth.guard';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./components/landing/landing.module').then(
-        (m) => m.LandingModule
-      ),
+    loadChildren: () => import('./components/landing/landing.module').then((m) => m.LandingModule),
   },
   {
     path: 'login',
@@ -16,35 +13,21 @@ export const routes: Routes = [
   {
     path: 'notfound',
     loadComponent() {
-      return import('./shared/notfound/notfound.component').then(
-        (m) => m.NotfoundComponent
-      );
+      return import('./shared/notfound/notfound.component').then((m) => m.NotfoundComponent);
     },
   },
   {
     path: 'homelogin',
     loadComponent() {
-      return import('./front-office/home-office/home-office.component').then(
-        (m) => m.HomeOfficeComponent
-      );
+      return import('./front-office/home-office/home-office.component').then((m) => m.HomeOfficeComponent);
     },
     canActivate: [authGuard],
     canActivateChild: [authGuard],
     children: [
-      // {
-      //   path: '',
-      //   loadComponent() {
-      //     return import(
-      //       './front-office/companies-list/companies-list.component'
-      //     ).then((c) => c.CompaniesListComponent);
-      //   },
-      // },
       {
         path: '',
         loadComponent() {
-          return import(
-            './front-office/invoices-list/invoices-list.component'
-          ).then((m) => m.InvoicesListComponent);
+          return import('./front-office/invoices-list/invoices-list.component').then((m) => m.InvoicesListComponent);
         },
         canActivate: [authGuard],
       },
