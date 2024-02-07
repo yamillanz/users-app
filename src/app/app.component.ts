3d6e75b0c5b9505f81aa8abcbbe8c125b2f46243
Yamil Lanz <yamil.lanz@gmail.com>
2024-02-07 16:14:41 -0400

    cleannig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
-import { LayoutService, AppConfig } from '../app/services/app.layout.service';
+// import { LayoutService, AppConfig } from '../app/services/app.layout.service';
 // import { authGuard } from './shared/guards/auth.guard';
 // import { PrimeBlocksModule } from './components/primeblocks/primeblocks.module';
 
@@ -10,23 +10,23 @@ import { LayoutService, AppConfig } from '../app/services/app.layout.service';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  providers: [LayoutService],
+  // providers: [LayoutService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   primengConfig = inject(PrimeNGConfig);
-  layoutService = inject(LayoutService);
+  // layoutService = inject(LayoutService);
 
   ngOnInit(): void {
-    const config: AppConfig = {
-      ripple: true,
-      inputStyle: 'outlined',
-      menuMode: 'overlay',
-      colorScheme: 'dark',
-      theme: 'saga-blue',
-      scale: 14,
-    };
+    // const config: AppConfig = {
+    //   ripple: true,
+    //   inputStyle: 'outlined',
+    //   menuMode: 'overlay',
+    //   colorScheme: 'dark',
+    //   theme: 'saga-blue',
+    //   scale: 14,
+    // };
     this.primengConfig.ripple = true;
     // this.layoutService.config = config;
   }
